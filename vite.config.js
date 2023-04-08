e1429ca206d9b8fd45a9fc34fe6a3c9e89385495
CursedHamster <viktoriia.harniuk.itis.2019@lpnu.ua>
2023-04-08 04:51:04 +0300

    fixed build error?

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
-    server: {
-      proxy: {
-        '/chat-messaging': {
-          target: 'http://localhost:8080',
-          ws: true
-        }
-      }
-    }
-  },
 });
