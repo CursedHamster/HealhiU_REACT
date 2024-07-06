2521339bdc7c65b0373f41263ad251aabb1609e5
CursedHamster <viktoriia.harniuk.itis.2019@lpnu.ua>
2024-07-06 16:42:53 +0300

    changed server url

--- src/components/config/axiosConfig.js
+++ src/components/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const url = "localhost:8080";
-const url = "16.170.253.71:8080";
+const url = "ec2-16-170-253-71.eu-north-1.compute.amazonaws.com:8080";
 // const url = "healthiuspring-production.up.railway.app";
 
 export default axios.create({
